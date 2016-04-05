00f5f2bede8c1f8a66ff780b44077ef541e738a5
Cesar Eduardo <pkgears@gmail.com>
2016-04-05 18:14:53 -0500

    Modelos, vistas, etc

---
+++ blog/app/controllers/articles_controller.rb
@@ -0,0 +1,10 @@
+class ArticlesController < ApplicationController
+  #GET /articles
+  def index
+    @articles = Article.all
+  end
+  # GET /articles/:id
+  def show
+    @article = Article.find(params[:id])
+  end
+end
